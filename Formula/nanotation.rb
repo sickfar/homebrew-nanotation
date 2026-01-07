821fdc6c83c0735514a5c1dcd6e4796b746dd296
sickfar <sickfar69@gmail.com>
2026-01-07 11:32:16 +0100

    Update formula with real SHAs and versioning

--- Formula/nanotation.rb
+++ Formula/nanotation.rb
@@ -1,25 +1,21 @@
 class Nanotation < Formula
   desc "Label and annotate your code directly from the terminal"
   homepage "https://github.com/sickfar/nanotation"
-  url "https://github.com/sickfar/nanotation/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "PENDING" # This will be replaced by the binary distribution block
+  version "0.1.0"
   license "MIT"
 
-  # Binary distribution for faster installation
-  # To update: brew eb nanotation (edit) and update URLs/SHAs after release
-  
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/sickfar/nanotation/releases/download/v0.1.0/nanotation-v0.1.0-aarch64-apple-darwin.tar.gz"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000" # Placeholder
+      url "https://github.com/sickfar/nanotation/releases/download/v#{version}/nanotation-v#{version}-aarch64-apple-darwin.tar.gz"
+      sha256 "d3bf6d6b8b9bc91facb2dee1842d3be4c872f4e29b962a98e06770c5cbb50a40"
     end
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/sickfar/nanotation/releases/download/v0.1.0/nanotation-v0.1.0-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000" # Placeholder
+      url "https://github.com/sickfar/nanotation/releases/download/v#{version}/nanotation-v#{version}-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "1b4a8988b63c645c48a0e05f080989fb570199d6db81b3d096ff07b69cac8cc7"
     elsif Hardware::CPU.arm?
-      url "https://github.com/sickfar/nanotation/releases/download/v0.1.0/nanotation-v0.1.0-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000" # Placeholder
+      url "https://github.com/sickfar/nanotation/releases/download/v#{version}/nanotation-v#{version}-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "ca0f5a9dce1bfb801d5cfc07a9b4d202a680ed972b173a37d25f8fd3023791d1"
     end
   end
 
