b536d556682a105ca9b345d13f382dc31d49f6c9
sickfar <sickfar69@gmail.com>
2026-01-13 13:05:05 +0100

    Update nanotation to version 0.2.0
    - Update version from 0.1.0 to 0.2.0
    - Update SHA256 checksums for new release artifacts
    - Remove Linux ARM64 support (not available in 0.2.0)
    - macOS ARM64: 73f4f93db41c4ff4b8ee9bbdfeb8bd06447d0eae2b16e458615b3b2eb61329a8
    - Linux x86_64: 7692bda4b12ccdd437efab729d98a7396abb668fb03b722113136e17433b5bef

--- Formula/nanotation.rb
+++ Formula/nanotation.rb
@@ -1,21 +1,18 @@
 class Nanotation < Formula
   desc "Label and annotate your code directly from the terminal"
   homepage "https://github.com/sickfar/nanotation"
-  version "0.1.0"
+  version "0.2.0"
   license "MIT"
 
   if OS.mac?
     if Hardware::CPU.arm?
       url "https://github.com/sickfar/nanotation/releases/download/v#{version}/nanotation-v#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "d3bf6d6b8b9bc91facb2dee1842d3be4c872f4e29b962a98e06770c5cbb50a40"
+      sha256 "73f4f93db41c4ff4b8ee9bbdfeb8bd06447d0eae2b16e458615b3b2eb61329a8"
     end
   elsif OS.linux?
     if Hardware::CPU.intel?
       url "https://github.com/sickfar/nanotation/releases/download/v#{version}/nanotation-v#{version}-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "1b4a8988b63c645c48a0e05f080989fb570199d6db81b3d096ff07b69cac8cc7"
-    elsif Hardware::CPU.arm?
-      url "https://github.com/sickfar/nanotation/releases/download/v#{version}/nanotation-v#{version}-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "ca0f5a9dce1bfb801d5cfc07a9b4d202a680ed972b173a37d25f8fd3023791d1"
+      sha256 "7692bda4b12ccdd437efab729d98a7396abb668fb03b722113136e17433b5bef"
     end
   end
 
